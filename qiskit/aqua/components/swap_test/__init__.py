597ff3fc61d8eb979b1d33bcfc2fa9ef9be7cb42
Ludwig Schmid <ludwig.schmid@tutanota.com>
2021-01-27 10:32:08 +0100

    Added files to perform SWAP test

---
+++ qiskit/aqua/components/swap_test/__init__.py
@@ -0,0 +1,22 @@
+"""
+SWAP Test (:mod:`qiskit.aqua.components.swap_test`)
+================================================
+Used to perform a swap test on two registers using the improved version
+with less swap gates
+
+.. currentmodule:: qiskit.aqua.components.swap_test
+
+SWAP test class
+======================
+
+.. autosummary::
+   :toctree: ../stubs/
+   :nosignatures:
+
+   SwapTest
+
+"""
+
+from .swap_test import SwapTest
+
+__all__ = ['SwapTest']
