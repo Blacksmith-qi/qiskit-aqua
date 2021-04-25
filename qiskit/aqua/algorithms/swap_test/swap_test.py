Bugfix: Consider normalisation of prepared state

qiskit/aqua/algorithms/swap_test/swap_test.py
@@ -109,10 +109,13 @@ class SwapTest(QuantumAlgorithm):
             self.run()
 
         counts = self._results.get_counts()
+        num_of_runs = sum(counts.values())
 
         # Postselect counts with ancillae in 11
         if qdf == True:
             counts = algorithms.linear_solvers.QDF._filter_results(counts)
+
+        successful_runs = sum(counts.values())
 
         # Starting post processing
         runs_pos = 0
@@ -123,14 +126,20 @@ class SwapTest(QuantumAlgorithm):
             for idx in range(self._regsize):
                 if state[idx] == '1' and state[idx + int(self._regsize)] == '1':
                     number_11 += 1
-            # DEBUG print(state + '  ' + str(hits))
+            # DEBUG 
+            # print(state + '  ' + str(hits))
+            # print(number_11)
+
             # If pairs of 00 or 11 is even -> equals 0 in clas ancilla
             if number_11 % 2 == 0:
                 runs_neg += hits
             else:
                 runs_pos += hits
         if (runs_neg + runs_pos) != 0:
-            prob = runs_pos / (runs_pos + runs_neg)
+            # Probability to successful rotation
+            prob_measure = successful_runs / num_of_runs
+            # Probability of successful measurement
+            prob = runs_pos / (runs_pos + runs_neg) / np.sqrt(prob_measure)
         else:
             prob = 0.5
         error = 2 * (1 - np.sqrt(abs(1-2 * prob)))