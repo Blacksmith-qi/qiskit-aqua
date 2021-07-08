88f2a18b252ada86ab81a0f82b25b5621a97a1dc
Ludwig Schmid <ludwig.schmid@tutanota.com>
2021-07-08 09:38:41 +0200

    Added and tested rotations

--- qiskit/aqua/components/reciprocals/__init__.py
+++ qiskit/aqua/components/reciprocals/__init__.py
@@ -46,5 +46,6 @@ from .long_division import LongDivision
 from .direct_rotation import DirectRotation
 from .direct_lookup_rotation import DirectLookupRotation
 from .sqrt_rot import SqrtRotation
+from .sqrt_inv import SqrtInvRotation
 
 __all__ = ['LookupRotation', 'LongDivision', 'Reciprocal', 'DirectRotation', DirectLookupRotation, SqrtRotation]
